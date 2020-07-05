2e0d584e49043c4cb170d8fa1063a3ba73fd1a10
2020-DEV-073 <katakotlin@gmail.com>
2020-07-05 21:34:16 +0200

    Added game activity

--- app/src/main/java/com/kata/tictactoeapp/view/GameActivity.kt
+++ app/src/main/java/com/kata/tictactoeapp/view/GameActivity.kt
@@ -1,12 +1,53 @@
 package com.kata.tictactoeapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.kata.tictactoeapp.R
 
+import com.kata.tictactoeapp.databinding.MainBinder
+import com.kata.tictactoeapp.viewmodel.GameViewModel
+
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var binding : MainBinder
+    private lateinit var viewModel : GameViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        binding.gameViewModel = viewModel
+        viewModel.init()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.winner.observe(this, Observer { winner ->
+            if (winner == "X") {
+                showToast("Player 1 win the game")
+                restartGame()
+
+            } else if (winner == "O") {
+                showToast("Player 2 win the game")
+                restartGame()
+            }
+        })
+
+        viewModel.areAllCellsFilled.observe(this, Observer { areAllCellsFilled ->
+            if (areAllCellsFilled) restartGame()
+        })
+    }
+
+    private fun showToast(value: String) {
+        Toast.makeText(this, value, Toast.LENGTH_LONG).show()
+    }
+
+    private fun restartGame() {
+        viewModel.init()
     }
 }
