3c4609ebb1385dde445f09a7e3dfe10b1160edb6
2020-DEV-073 <katakotlin@gmail.com>
2020-07-05 20:53:48 +0200

    Created packages and folder structure

--- app/src/main/java/com/kata/tictactoeapp/view/GameActivity.kt
+++ app/src/main/java/com/kata/tictactoeapp/view/GameActivity.kt
@@ -1,9 +1,10 @@
-package com.kata.tictactoeapp
+package com.kata.tictactoeapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kata.tictactoeapp.R
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
